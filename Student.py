3f76c2d10ad3cc75fc5f1c4620de10b93502a1a8
zibin <chenzibin2017@outlook.com>
2024-09-23 15:08:41 -0400

    students.txt

--- Student.py
+++ Student.py
@@ -26,17 +26,48 @@ class StudentNode(object):
         self.next = None
 
     def getNext(self):
+        """
+        Get the next student node in the LL
+        :return:
+        """
         return self.next
 
     def getStudent(self):
+        """
+        Get the student instance in the node
+        :return:
+        """
         return self.student
 
-    def setStudent(self, student):
-        self.student = student
-
     def setNext(self, new_next):
+        """
+        Set the next student node for the current node
+        :param new_next:
+        :return:
+        """
         self.next = new_next
+
+    def replicate(self):
+        """
+        Create a replicate node with the same student object.
+        The new node will have the reference to the same student object.
+        Note: the replicated node will have next pointer points to None.
+        :return: The replicated node
+        """
+        rep = StudentNode("", 0, 0, 0)
+        new_instances = rep.student
+        rep.student = self.student
+        del new_instances
+        return rep
 
     def __str__(self):
         return str(self.student)
+
+
+if __name__ == '__main__':
+    node = StudentNode("test", 1, 1, 1)
+    rep_node = node.replicate()
+
+    assert rep_node.next is None
+    assert rep_node.getStudent() is node.getStudent()
 
